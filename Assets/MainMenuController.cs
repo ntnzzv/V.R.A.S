110a6e685a512d5060636032958acbbf544c3131
Natan Z <56848492+ntnzzv@users.noreply.github.com>
2022-05-27 12:58:38 +0300

    menu changes

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
-public void StartBtn()
-{
-         SceneManager.LoadScene("main", LoadSceneMode.Single);
-   // SceneManager.LoadScene("main");
-}
 
+    public void StartBtn()
+    {
+        InputField AgeInput = GameObject.FindWithTag("AgeField").GetComponent<InputField>();
+        Text ageText = AgeInput.GetComponent<Text>();
+        if (!string.IsNullOrEmpty(ageText.text))
+        {
+          
+
+        }
+        SceneManager.LoadScene("main", LoadSceneMode.Single);
+    }
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
 
 }
